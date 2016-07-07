409195845a93b24fd7c6bcf78e678d2302a48ec6
Anthony Parsch <anthonyparsch@gmail.com>
2016-07-07 11:36:33 -0700

    Completed assignment 7

---
+++ Assignment_9/src/com/parsch/anthony/BankAccount.java
@@ -0,0 +1,19 @@
+package com.parsch.anthony;
+
+public class BankAccount 
+{
+	private String accountNumber;
+	private float currentBalance;
+	
+	public String accountHolder;
+	
+	public void deposit (float change)
+	{
+		
+	}
+	
+	public void withdrawl (float change)
+	{
+		
+	}
+}
